99f20fd8870adfc03e56905f33857cf57f5795ca
Brandon Thomas <brandonvthomas97@gmail.com>
2020-08-20 14:35:27 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,11 @@
 # Write your #display_rainbow method here
+def display_rainbow (color)
+puts color = "R: #{color[red]}, O: #{color[orange]}, Y: #{color[yellow]}, G: #{color[green]}, B: #{color[blue]}, I: #{color[indigo]}, V: #{color[violet]}"
+"The first color in the display_rainbow is #{color[0]}"
+"The second color in the display_rainbow is #{color[1]}"
+"The third color in the display_rainbow is #{color[2]}"
+"The fourth color in the display_rainbow is #{color[3]}"
+"The fifth color in the display_rainbow is #{color[4]}"
+"The sixth color in the display_rainbow is #{color[5]}"
+"The seventh color in the display_rainbow is #{color[6]}"
+end
